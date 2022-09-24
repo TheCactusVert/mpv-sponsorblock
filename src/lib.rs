b35f39f2fa4bd85def61edc69542f71533c9b47c
TheCactusVert <kevin@gavrois.fr>
2022-09-24 22:28:16 +0200

    chore: moving mpv api to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+mod mpv;
+
+use crate::mpv::*;
+
 use std::ffi::{CStr, CString};
 use std::os::raw::{c_char, c_int, c_void};
 
@@ -6,47 +10,6 @@ use sponsor_block::{AcceptedActions, AcceptedCategories, Client, Segment};
 
 // This should be random, treated like a password, and stored across sessions
 const USER_ID: &str = include_str!("../user.in");
-
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct mpv_handle {
-    _unused: [u8; 0],
-}
-
-#[allow(non_camel_case_types)]
-type mpv_event_id = c_int;
-
-pub const MPV_EVENT_SHUTDOWN: mpv_event_id = 1;
-pub const MPV_EVENT_FILE_LOADED: mpv_event_id = 8;
-pub const MPV_EVENT_PROPERTY_CHANGE: mpv_event_id = 22;
-
-#[allow(non_camel_case_types)]
-pub type mpv_format = c_int;
-
-pub const MPV_FORMAT_DOUBLE: mpv_format = 5;
-
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct mpv_event {
-    pub event_id: mpv_event_id,
-    pub error: c_int,
-    pub reply_userdata: u64,
-    pub data: *mut c_void,
-}
-
-extern "C" {
-    pub fn mpv_wait_event(ctx: *mut mpv_handle, timeout: f64) -> *mut mpv_event;
-    pub fn mpv_client_name(ctx: *mut mpv_handle) -> *const c_char;
-    pub fn mpv_get_property_string(ctx: *mut mpv_handle, name: *const c_char) -> *mut c_char;
-    pub fn mpv_free(data: *mut c_void);
-    pub fn mpv_observe_property(
-        mpv: *mut mpv_handle,
-        reply_userdata: u64,
-        name: *const c_char,
-        format: mpv_format,
-    ) -> c_int;
-    pub fn mpv_unobserve_property(mpv: *mut mpv_handle, registered_reply_userdata: u64) -> c_int;
-}
 
 pub const YOUTUBE_REPLY_USERDATA: u64 = 1;
 
