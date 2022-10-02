ded4d3b3735b4e5f42fab55e2058bbffd7cc536f
TheCactusVert <kevin@gavrois.fr>
2022-10-02 13:19:06 +0200

    chore: reorganise code

--- src/mpv/mod.rs
+++ src/mpv/mod.rs
@@ -1,56 +1,9 @@
+mod raw;
+
 use std::ffi::{CStr, CString};
 use std::os::raw::{c_char, c_int, c_void};
 
 use anyhow::Result;
-
-mod raw {
-    use std::os::raw::{c_char, c_int, c_void};
-
-    #[repr(i32)]
-    #[allow(dead_code)]
-    #[derive(Copy, Clone, PartialEq)]
-    pub enum mpv_event_id {
-        Shutdown = 1,
-        StartFile = 6,
-        EndFile = 7,
-        PropertyChange = 22,
-    }
-
-    #[repr(i32)]
-    #[allow(dead_code)]
-    #[derive(Copy, Clone, PartialEq)]
-    pub enum mpv_format {
-        None = 0,
-        String = 1,
-        OSDString = 2,
-        Flag = 3,
-        Int64 = 4,
-        Double = 5,
-        NodeArray = 7,
-        NodeMap = 8,
-        ByteArray = 9,
-    }
-
-    #[repr(C)]
-    pub struct mpv_handle {
-        _unused: [u8; 0],
-    }
-
-    #[repr(C)]
-    pub struct mpv_event {
-        pub event_id: mpv_event_id,
-        pub error: c_int,
-        pub reply_userdata: u64,
-        pub data: *mut c_void,
-    }
-
-    #[repr(C)]
-    pub struct mpv_event_property {
-        pub name: *const c_char,
-        pub format: mpv_format,
-        pub data: *mut c_void,
-    }
-}
 
 pub type MpvEventID = raw::mpv_event_id;
 pub type MpvFormat = raw::mpv_format;
