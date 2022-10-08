fcfb3f80516125bf4add99d1882cabd495cbce06
TheCactusVert <kevin@gavrois.fr>
2022-10-08 16:20:35 +0200

    chore: updating regex for youtuve id

--- src/utils.rs
+++ src/utils.rs
@@ -20,7 +20,7 @@ pub fn get_data(url: &str) -> Result<Vec<u8>, curl::Error> {
 
 pub fn get_youtube_id(path: &str) -> Option<String> {
     // I don't uderstand this crap but it's working (almost)
-    let regex = Regex::new(r"https?://(?:www\.)?(?:youtu\.be|youtube\.com|piped\.kavin\.rocks)/(?:v/|watch\?v=|embed/)?([A-Za-z0-9-_]+)").ok()?;
+    let regex = Regex::new(r"https?://(?:www\.)?(?:youtu\.be|youtube\.com|piped\.kavin\.rocks)/(?:v/|watch\?v=|embed/)?([A-Za-z0-9-_]{11})").ok()?;
     let capture = regex.captures(path)?;
     capture.get(1).map(|m| m.as_str().to_string())
 }
@@ -30,9 +30,13 @@ mod tests {
     use super::get_youtube_id;
 
     #[test]
-    fn test_yt_id() {
+    fn parse_youtube_id() {
         assert_eq!(
             get_youtube_id("https://youtu.be/dQw4w9WgXcQ"),
+            Some("dQw4w9WgXcQ".to_string())
+        );
+        assert_eq!(
+            get_youtube_id("http://youtu.be/dQw4w9WgXcQ"),
             Some("dQw4w9WgXcQ".to_string())
         );
         assert_eq!(
@@ -45,6 +49,10 @@ mod tests {
         );
         assert_eq!(
             get_youtube_id("https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+            Some("dQw4w9WgXcQ".to_string())
+        );
+        assert_eq!(
+            get_youtube_id("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=20s"),
             Some("dQw4w9WgXcQ".to_string())
         );
         assert_eq!(
