88aeecda3c607d46af534f615a75e9855e1ffd3c
TheCactusVert <kevin@gavrois.fr>
2022-10-16 15:53:10 +0200

    chore: cleaning code

--- src/mpv/mod.rs
+++ src/mpv/mod.rs
@@ -1,44 +1,55 @@
 mod ffi;
 
+use ffi::*;
+
 use std::any::TypeId;
 use std::ffi::{c_void, CStr, CString, NulError};
 use std::fmt;
 use std::str::Utf8Error;
 
-pub type Format = ffi::mpv_format;
-pub type RawHandle = *mut ffi::mpv_handle;
+pub type Format = mpv_format;
+pub type RawHandle = *mut mpv_handle;
 pub type ReplyUser = u64;
-pub struct Handle(*mut ffi::mpv_handle);
-pub struct EventStartFile(*mut ffi::mpv_event_start_file);
-pub struct EventProperty(*mut ffi::mpv_event_property);
-pub struct EventHook(*mut ffi::mpv_event_hook);
+pub struct Handle(*mut mpv_handle);
+pub struct EventStartFile(*mut mpv_event_start_file);
+pub struct EventProperty(*mut mpv_event_property);
+pub struct EventHook(*mut mpv_event_hook);
 
 #[derive(Debug)]
-pub struct Error(ffi::mpv_error);
+pub struct Error(mpv_error);
 pub type Result<T> = std::result::Result<T, Error>;
 
+macro_rules! convert_result {
+    ($f:expr) => {
+        match $f {
+            mpv_error::SUCCESS => Ok(()),
+            e => Err(Error::new(e)),
+        }
+    };
+}
+
 impl Error {
-    fn new(error: ffi::mpv_error) -> Self {
+    fn new(error: mpv_error) -> Self {
         Self(error)
     }
 }
 
 impl From<NulError> for Error {
     fn from(_: NulError) -> Self {
-        Self::new(ffi::mpv_error::GENERIC)
+        Self::new(mpv_error::GENERIC)
     }
 }
 
 impl From<Utf8Error> for Error {
     fn from(_: Utf8Error) -> Self {
-        Self::new(ffi::mpv_error::GENERIC)
+        Self::new(mpv_error::GENERIC)
     }
 }
 
 impl std::error::Error for Error {
     fn description(&self) -> &str {
         unsafe {
-            CStr::from_ptr(ffi::mpv_error_string(self.0))
+            CStr::from_ptr(mpv_error_string(self.0))
                 .to_str()
                 .unwrap_or("unknow error")
         }
@@ -48,7 +59,7 @@ impl std::error::Error for Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let e_str = unsafe {
-            CStr::from_ptr(ffi::mpv_error_string(self.0))
+            CStr::from_ptr(mpv_error_string(self.0))
                 .to_str()
                 .unwrap_or("unknow error")
         };
@@ -108,7 +119,7 @@ impl Handle {
 
     pub fn wait_event(&self, timeout: f64) -> (ReplyUser, Result<Event>) {
         unsafe {
-            let mpv_event = ffi::mpv_wait_event(self.0, timeout);
+            let mpv_event = mpv_wait_event(self.0, timeout);
 
             if mpv_event.is_null() {
                 return (0, Ok(Event::None));
@@ -116,37 +127,35 @@ impl Handle {
 
             let mpv_reply: ReplyUser = (*mpv_event).reply_userdata;
 
-            if (*mpv_event).error != ffi::mpv_error::SUCCESS {
+            if (*mpv_event).error != mpv_error::SUCCESS {
                 return (mpv_reply, Err(Error::new((*mpv_event).error)));
             }
 
             (
                 mpv_reply,
                 Ok(match (*mpv_event).event_id {
-                    ffi::mpv_event_id::SHUTDOWN => Event::Shutdown,
-                    ffi::mpv_event_id::LOG_MESSAGE => Event::LogMessage,
-                    ffi::mpv_event_id::GET_PROPERTY_REPLY => Event::GetPropertyReply(EventProperty::from_ptr(
-                        (*mpv_event).data as *mut ffi::mpv_event_property,
-                    )),
-                    ffi::mpv_event_id::SET_PROPERTY_REPLY => Event::SetPropertyReply,
-                    ffi::mpv_event_id::COMMAND_REPLY => Event::CommandReply,
-                    ffi::mpv_event_id::START_FILE => Event::StartFile(EventStartFile::from_ptr(
-                        (*mpv_event).data as *mut ffi::mpv_event_start_file,
-                    )),
-                    ffi::mpv_event_id::END_FILE => Event::EndFile,
-                    ffi::mpv_event_id::FILE_LOADED => Event::FileLoaded,
-                    ffi::mpv_event_id::CLIENT_MESSAGE => Event::ClientMessage,
-                    ffi::mpv_event_id::VIDEO_RECONFIG => Event::VideoReconfig,
-                    ffi::mpv_event_id::AUDIO_RECONFIG => Event::AudioReconfig,
-                    ffi::mpv_event_id::SEEK => Event::Seek,
-                    ffi::mpv_event_id::PLAYBACK_RESTART => Event::PlaybackRestart,
-                    ffi::mpv_event_id::PROPERTY_CHANGE => Event::PropertyChange(EventProperty::from_ptr(
-                        (*mpv_event).data as *mut ffi::mpv_event_property,
-                    )),
-                    ffi::mpv_event_id::QUEUE_OVERFLOW => Event::QueueOverflow,
-                    ffi::mpv_event_id::HOOK => {
-                        Event::Hook(EventHook::from_ptr((*mpv_event).data as *mut ffi::mpv_event_hook))
+                    mpv_event_id::SHUTDOWN => Event::Shutdown,
+                    mpv_event_id::LOG_MESSAGE => Event::LogMessage,
+                    mpv_event_id::GET_PROPERTY_REPLY => {
+                        Event::GetPropertyReply(EventProperty::from_ptr((*mpv_event).data as *mut mpv_event_property))
                     }
+                    mpv_event_id::SET_PROPERTY_REPLY => Event::SetPropertyReply,
+                    mpv_event_id::COMMAND_REPLY => Event::CommandReply,
+                    mpv_event_id::START_FILE => {
+                        Event::StartFile(EventStartFile::from_ptr((*mpv_event).data as *mut mpv_event_start_file))
+                    }
+                    mpv_event_id::END_FILE => Event::EndFile,
+                    mpv_event_id::FILE_LOADED => Event::FileLoaded,
+                    mpv_event_id::CLIENT_MESSAGE => Event::ClientMessage,
+                    mpv_event_id::VIDEO_RECONFIG => Event::VideoReconfig,
+                    mpv_event_id::AUDIO_RECONFIG => Event::AudioReconfig,
+                    mpv_event_id::SEEK => Event::Seek,
+                    mpv_event_id::PLAYBACK_RESTART => Event::PlaybackRestart,
+                    mpv_event_id::PROPERTY_CHANGE => {
+                        Event::PropertyChange(EventProperty::from_ptr((*mpv_event).data as *mut mpv_event_property))
+                    }
+                    mpv_event_id::QUEUE_OVERFLOW => Event::QueueOverflow,
+                    mpv_event_id::HOOK => Event::Hook(EventHook::from_ptr((*mpv_event).data as *mut mpv_event_hook)),
                     _ => Event::None,
                 }),
             )
@@ -155,7 +164,7 @@ impl Handle {
 
     pub fn client_name(&self) -> String {
         unsafe {
-            let c_name = ffi::mpv_client_name(self.0);
+            let c_name = mpv_client_name(self.0);
             let c_str = CStr::from_ptr(c_name);
             let str_slice: &str = c_str.to_str().unwrap_or("unknown");
             str_slice.to_owned()
@@ -166,63 +175,40 @@ impl Handle {
         let c_name = CString::new(name.into())?;
 
         unsafe {
-            let c_path = ffi::mpv_get_property_string(self.0, c_name.as_ptr());
+            let c_path = mpv_get_property_string(self.0, c_name.as_ptr());
             if c_path.is_null() {
-                return Err(Error::new(ffi::mpv_error::PROPERTY_NOT_FOUND));
+                return Err(Error::new(mpv_error::PROPERTY_NOT_FOUND));
             }
             let c_str = CStr::from_ptr(c_path);
             let str_buf = c_str.to_str().map(|s| s.to_owned());
-            ffi::mpv_free(c_path as *mut c_void);
+            mpv_free(c_path as *mut c_void);
             Ok(str_buf?) // Meh it shouldn't fail
         }
     }
 
     pub fn set_property<S: Into<String>, T>(&self, name: S, format: Format, mut data: T) -> Result<()> {
         let c_name = CString::new(name.into())?;
-
-        unsafe {
-            let data: *mut c_void = &mut data as *mut _ as *mut c_void;
-            match ffi::mpv_set_property(self.0, c_name.as_ptr(), format, data) {
-                ffi::mpv_error::SUCCESS => Ok(()),
-                e => Err(Error::new(e)),
-            }
-        }
+        let data: *mut c_void = &mut data as *mut _ as *mut c_void;
+        unsafe { convert_result!(mpv_set_property(self.0, c_name.as_ptr(), format, data)) }
     }
 
     pub fn observe_property<S: Into<String>>(&self, reply_userdata: ReplyUser, name: S, format: Format) -> Result<()> {
         let c_name = CString::new(name.into())?;
-
-        unsafe {
-            match ffi::mpv_observe_property(self.0, reply_userdata, c_name.as_ptr(), format) {
-                ffi::mpv_error::SUCCESS => Ok(()),
-                e => Err(Error::new(e)),
-            }
-        }
+        unsafe { convert_result!(mpv_observe_property(self.0, reply_userdata, c_name.as_ptr(), format)) }
     }
 
     pub fn hook_add<S: Into<String>>(&self, reply_userdata: ReplyUser, name: S, priority: i32) -> Result<()> {
         let c_name = CString::new(name.into())?;
-
-        unsafe {
-            match ffi::mpv_hook_add(self.0, reply_userdata, c_name.as_ptr(), priority) {
-                ffi::mpv_error::SUCCESS => Ok(()),
-                e => Err(Error::new(e)),
-            }
-        }
+        unsafe { convert_result!(mpv_hook_add(self.0, reply_userdata, c_name.as_ptr(), priority)) }
     }
 
     pub fn hook_continue(&self, id: u64) -> Result<()> {
-        unsafe {
-            match ffi::mpv_hook_continue(self.0, id) {
-                ffi::mpv_error::SUCCESS => Ok(()),
-                e => Err(Error::new(e)),
-            }
-        }
+        unsafe { convert_result!(mpv_hook_continue(self.0, id)) }
     }
 }
 
 impl EventStartFile {
-    fn from_ptr(event: *mut ffi::mpv_event_start_file) -> Self {
+    fn from_ptr(event: *mut mpv_event_start_file) -> Self {
         assert!(!event.is_null());
         Self(event)
     }
@@ -233,7 +219,7 @@ impl EventStartFile {
 }
 
 impl EventProperty {
-    fn from_ptr(event: *mut ffi::mpv_event_property) -> Self {
+    fn from_ptr(event: *mut mpv_event_property) -> Self {
         assert!(!event.is_null());
         Self(event)
     }
@@ -241,16 +227,16 @@ impl EventProperty {
     pub fn get_data<T: Copy + 'static>(&self) -> Option<T> {
         unsafe {
             let format = (*self.0).format;
-            if format == ffi::mpv_format::NONE {
+            if format == mpv_format::NONE {
                 return None;
             }
 
             let type_id = TypeId::of::<T>();
             if type_id == TypeId::of::<i64>() {
-                assert!(format == ffi::mpv_format::INT64, "The format is not of type i64!");
+                assert!(format == mpv_format::INT64, "The format is not of type i64!");
                 Some(*((*self.0).data as *mut T))
             } else if type_id == TypeId::of::<f64>() {
-                assert!(format == ffi::mpv_format::DOUBLE, "The format is not of type f64!");
+                assert!(format == mpv_format::DOUBLE, "The format is not of type f64!");
                 Some(*((*self.0).data as *mut T))
             } else {
                 panic!("Unsupported format!");
@@ -260,7 +246,7 @@ impl EventProperty {
 }
 
 impl EventHook {
-    fn from_ptr(event: *mut ffi::mpv_event_hook) -> Self {
+    fn from_ptr(event: *mut mpv_event_hook) -> Self {
         assert!(!event.is_null());
         Self(event)
     }
