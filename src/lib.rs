7f3a1aae051d413b31dec232b7e8d8d638234123
TheCactusVert <kevin@gavrois.fr>
2022-10-28 11:22:40 +0200

    chore: hook continue should never fail

--- src/lib.rs
+++ src/lib.rs
@@ -80,10 +80,7 @@ extern "C" fn mpv_open_cplugin(handle: RawHandle) -> std::os::raw::c_int {
                 // Non blocking operation might be better, but risky on short videos ?!
                 actions.load_segments(&mpv_handle, &config);
                 // Unblock MPV and continue
-                if let Err(e) = mpv_handle.hook_continue(event.get_id()) {
-                    log::error!("Failed to continue hook: {}.", e);
-                    return -1;
-                }
+                mpv_handle.hook_continue(event.get_id()).unwrap();
             }
             (reply, Ok(event)) => {
                 log::trace!("Ignoring {} event on reply {}.", event, reply);
