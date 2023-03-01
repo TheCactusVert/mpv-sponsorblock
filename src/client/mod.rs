d04c95af7e75f6446c35d72c6575d1ae575fddbf
TheCactusVert <kevin@gavrois.fr>
2023-03-01 15:11:33 +0100

    refactor: readability

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -77,14 +77,11 @@ impl Client {
     fn start_file(&mut self) -> Result<()> {
         let path: String = self.get_property(NAME_PROP_PATH)?;
         if let Some(id) = self.get_youtube_id(&path) {
-            let parent = self.name();
+            let parent = self.name().into();
+            let child = format!("{}-worker", parent);
+            let client = self.create_client(child)?;
 
-            self.worker.start(
-                self.create_client(format!("{}-worker", parent))?,
-                parent.to_string(),
-                self.config.clone(),
-                id.to_string(),
-            );
+            self.worker.start(client, parent, self.config.clone(), id.into());
             self.observe_property(REPL_PROP_TIME, NAME_PROP_TIME, f64::MPV_FORMAT)?;
             self.observe_property(REPL_PROP_MUTE, NAME_PROP_MUTE, bool::MPV_FORMAT)?;
         }
